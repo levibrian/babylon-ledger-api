c93c1272b26a8a62e3d98ac2e4ab2dd11e6957af
levibrian <bra.levi@gmail.com>
2022-08-06 19:33:46 +0200

    268 270: created and imported github actions and workflows. also created base src directory with empty solution.

---
+++ .github/actions/terraform-apply.sh
@@ -0,0 +1,65 @@
+﻿set -e 
+BASE_FOLDER=$(pwd)
+
+log_action() {
+    echo "${1^^} ..."
+}
+
+log_key_value_pair() {
+    echo "    $1: $2"
+}
+
+set_up_aws_user_credentials() {
+    unset AWS_SESSION_TOKEN
+    export AWS_DEFAULT_REGION=$1
+    export AWS_ACCESS_KEY_ID=$2
+    export AWS_SECRET_ACCESS_KEY=$3
+}
+
+log_action "planning terraform"
+
+log_key_value_pair "working-folder" $BASE_FOLDER
+
+REGION="$1"
+log_key_value_pair "region" "$REGION"
+
+ACCESS_KEY="$2"
+log_key_value_pair "access-key" "$ACCESS_KEY"
+
+SECRET_KEY="$3"
+
+TFM_FOLDER=$4
+log_key_value_pair "terraform-folder" $TFM_FOLDER
+
+ENVIRONMENT=$5
+log_key_value_pair "environment" $ENVIRONMENT
+
+STACK_NAME=$6
+log_key_value_pair "stack-name" $STACK_NAME
+
+TERRAFORM_PLAN_FILE=$7
+log_key_value_pair "terraform-plan-file" $TERRAFORM_PLAN_FILE
+
+set_up_aws_user_credentials $REGION $ACCESS_KEY $SECRET_KEY
+
+WORKING_FOLDER="$BASE_FOLDER/$TFM_FOLDER"
+
+cd $WORKING_FOLDER
+
+if [ "$STACK_NAME" = "" ]; then 
+    terraform workspace select $ENVIRONMENT || terraform workspace new $ENVIRONMENT
+    
+    terraform init -backend-config="access_key=$ACCESS_KEY" -backend-config="secret_key=$SECRET_KEY"
+    
+    terraform apply "$TERRAFORM_PLAN_FILE"
+    
+else
+    terraform workspace select $STACK_NAME || terraform workspace new $STACK_NAME
+
+    terraform init -backend-config="access_key=$ACCESS_KEY" -backend-config="secret_key=$SECRET_KEY"
+
+    terraform apply "$TERRAFORM_PLAN_FILE"
+fi
+
+
+cd $BASE_FOLDER
---
+++ .github/actions/terraform-init-validate.sh
@@ -0,0 +1,26 @@
+﻿set -e 
+WORKING_FOLDER=$(pwd)
+
+log_action() {
+    echo "${1^^} ..."
+}
+
+log_key_value_pair() {
+    echo "    $1: $2"
+}
+
+log_action "validating terraform"
+TFM_PATH=$1
+log_key_value_pair "tfm-path" $TFM_PATH
+ACCESS_KEY=$2
+log_key_value_pair "access-key" $ACCESS_KEY
+SECRET_KEY=$3
+BACKEND=$4
+log_key_value_pair "use-backend" $BACKEND
+
+cd $WORKING_FOLDER/$TFM_PATH
+
+terraform init -backend=$BACKEND -backend-config="access_key=$ACCESS_KEY" -backend-config="secret_key=$SECRET_KEY"
+terraform validate -no-color
+
+cd $WORKING_FOLDER
